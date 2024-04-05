8294b836a9611cdd5a89b0bab3ea6b4201e1d287
xslizik <99902995+xslizik@users.noreply.github.com>
2024-04-05 12:23:09 +0200

    Update app.py

--- dvf/app.py
+++ dvf/app.py
@@ -5,6 +5,8 @@ from endpoints.vulnerable_routes import vulnerable
 from endpoints.secure_routes import secure
 from db import init_db
 
+from models import sports_events, logins
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{env["DB_USER"]}:{env["PASSWORD"]}@{env["HOST"]}:{env["PORT"]}/{env["DATABASE"]}'
 
@@ -12,3 +14,9 @@ db = init_db(app)
 
 app.register_blueprint(vulnerable)
 app.register_blueprint(secure)
+
+with app.app_context():
+    db.create_all()
+    db.session.add_all(sports_events)
+    db.session.add_all(logins)
+    db.session.commit()
